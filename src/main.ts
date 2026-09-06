13ab77eb6e8e40c4fc4e1726cda4830cefdca4aa
kquan2312 <kquan2846@gmail.com>
2026-09-07 02:08:35 +0700

    sửa deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
+
