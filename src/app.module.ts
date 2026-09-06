d9a21635e192d0e3fdf1fcca146d23a0950d8ae3
kquan2312 <kquan2846@gmail.com>
2026-09-07 02:28:34 +0700

    xoa db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayfsModule } from './payfs/payfs.module';
-import { TransactionsModule } from './transactions/transactions.module';
 import { NotifierModule } from './notifier/notifier.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { PayfsGateway } from './payfs/gateway';
-
 
 @Module({
-  imports: [PayfsModule, TransactionsModule, NotifierModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-  ],
+  imports: [PayfsModule, NotifierModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
